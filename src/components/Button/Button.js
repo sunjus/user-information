5f285ca93e52c2a8f34229234082fad0b51b1a68
sunjus <sunju.shin1108@gmail.com>
2021-08-22 15:41:55 +0300

    Added style at button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
+import style from "./button.module.css";
 
 const Button = ({ id }) => {
   return (
-    <div>
+    <div className={style.btn}>
       <Link to={`/${id}`}>
         <button>More Details</button>
       </Link>
