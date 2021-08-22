449f4398bbe58e6f6afbc434673e9391134f7745
sunjus <sunju.shin1108@gmail.com>
2021-08-22 14:54:39 +0300

    Fetched and displayed users List

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,7 +1,32 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 const Users = () => {
-  return <div>Users List</div>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    setData(null);
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then((response) => response.json())
+      .then((parsedData) => setData(parsedData))
+      .catch((error) => console.log(error));
+  }, []);
+  console.log(data);
+  return (
+    <div>
+      {data ? (
+        data.map((user) => (
+          <div key={user.id}>
+            {user.name[0]}
+            {user.name}
+            {user.username}
+            {user.website}
+          </div>
+        ))
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  );
 };
 
 export default Users;
