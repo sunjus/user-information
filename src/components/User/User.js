1ef9e8976b22f23961a8974e5a606b1e82032302
sunjus <sunju.shin1108@gmail.com>
2021-08-22 15:06:25 +0300

    Divided component for each user info

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Button from "../Button";
 
-const User = () => {
-  return <div>A user</div>;
+const User = ({ character, name, username, website, id }) => {
+  return (
+    <div>
+      <div>
+        <div>{character}</div>
+        <div>
+          <h5>{name}</h5>
+          <p>{username}</p>
+          <p>
+            <a href={`${website}`}>{website}</a>
+          </p>
+        </div>
+        <Button id={id} />
+      </div>
+    </div>
+  );
 };
 
 export default User;
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import User from "../User";
 
 const Users = () => {
   const [data, setData] = useState(null);
@@ -10,17 +11,19 @@ const Users = () => {
       .then((parsedData) => setData(parsedData))
       .catch((error) => console.log(error));
   }, []);
-  console.log(data);
+  //console.log(data);
   return (
     <div>
       {data ? (
         data.map((user) => (
-          <div key={user.id}>
-            {user.name[0]}
-            {user.name}
-            {user.username}
-            {user.website}
-          </div>
+          <User
+            key={user.id}
+            character={user.name[0]}
+            name={user.name}
+            username={user.username}
+            website={user.website}
+            id={user.id}
+          />
         ))
       ) : (
         <div>Loading...</div>
